99f4aa37bbae881677c286a41eeb4e4b4a1037db
qinjoshua <qinjoshuac@outlook.com>
2022-04-10 05:20:17 -0400

    Modified loca-vision, controllers now use loca vision

--- loca_vision/gcloud.py
+++ loca_vision/gcloud.py
@@ -15,6 +15,8 @@ from .wiki_parser import headers
 from tqdm import tqdm
 import hashlib
 import re
+import base64
+import hashlib
 
 config = {**dotenv_values(".env"), **os.environ}
 
@@ -43,8 +45,91 @@ def upload_images_from_monuments(
                 except ConnectionError as e:
                     print("Could not read image", url, ", skipping")
 
-    return monuments
+    return
 
+
+def upload_base64_image(b64: str) -> str:
+    """Uploads an image in base64 to the GC bucket and returns a URI pointing to the image."""
+    fn = hashlib.md5(b64).hexdigest()[:15]
+    uri = f"{fn}.jpeg"
+    data = b64.split('base64,')[1]
+    bin_data = base64.b64encode(data)
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(config["IMAGE_BUCKET"])
+    blob = bucket.blob(uri)
+    blob.upload_from_string(bin_data)
+    return f"gs://{config['IMAGE_BUCKET']}/{uri}"
+
+
+def create_product(product_id, product_display_name):
+    client = vision.ProductSearchClient()
+
+    # A resource that represents Google Cloud Platform location.
+    location_path = f"projects/{config['PROJECT_ID']}/locations/us-east1"
+
+    # Create a product with the product specification in the region.
+    # Set product display name and product category.
+    product = vision.Product(
+        display_name=product_display_name,
+        product_category='general-v1')
+
+    # The response is the product with the `name` field populated.
+    response = client.create_product(
+        parent=location_path,
+        product=product,
+        product_id=product_id)
+
+    add_product_to_product_set(product_id)
+
+    # Display the product information.
+    print('Product name: {}'.format(response.name))
+
+def create_reference_image(product_id, reference_image_id, gcs_uri):
+    """Create a reference image.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        reference_image_id: Id of the reference image.
+        gcs_uri: Google Cloud Storage path of the input image.
+    """
+    client = vision.ProductSearchClient()
+
+    # Get the full path of the product.
+    product_path = client.product_path(
+        project=config['PROJECT_ID'], location='us-east1', product=product_id)
+
+    # Create a reference image.
+    reference_image = vision.ReferenceImage(uri=gcs_uri)
+
+    # The response is the reference image with `name` populated.
+    image = client.create_reference_image(
+        parent=product_path,
+        reference_image=reference_image,
+        reference_image_id=reference_image_id)
+
+def add_product_to_product_set(product_id):
+    """Add a product to a product set.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        product_set_id: Id of the product set.
+    """
+    client = vision.ProductSearchClient()
+
+    # Get the full path of the product set.
+    product_set_path = client.product_set_path(
+        project=config['PROJECT_ID'], location='us_east1',
+        product_set=config['PRODUCT_SET_ID'])
+
+    # Get the full path of the product.
+    product_path = client.product_path(
+        project=config['PROJECT_SET'], location='us_east1', product=product_id)
+
+    # Add the product to the product set.
+    client.add_product_to_product_set(
+        name=product_set_path, product=product_path)
 
 def monuments_to_csv(monuments: Sequence[WikiMonument]) -> str:
     """Reads the monuments into a CSV suitable for input and return it."""
@@ -126,11 +211,7 @@ def upload_product_set(monuments: Sequence[WikiMonument]):
 
 
 def get_similar_products_file(
-    project_id,
-    location,
-    product_set_id,
-    product_category,
-    file_path,
+    b64,
     filter,
     max_results,
 ):
@@ -153,19 +234,18 @@ def get_similar_products_file(
     image_annotator_client = vision.ImageAnnotatorClient()
 
     # Read the image as a stream of bytes.
-    with open(file_path, "rb") as image_file:
-        content = image_file.read()
+    content = base64.b64encode(b64.split['base64,'][1])
 
     # Create annotate image request along with product search feature.
     image = vision.Image(content=content)
 
     # product search specific parameters
     product_set_path = product_search_client.product_set_path(
-        project=project_id, location=location, product_set=product_set_id
+        project='loca-346705', location='us-east-1', product_set=config['PRODUCT_SET_ID']
     )
     product_search_params = vision.ProductSearchParams(
         product_set=product_set_path,
-        product_categories=[product_category],
+        product_categories=['general-v1'],
         filter=filter,
     )
     image_context = vision.ImageContext(product_search_params=product_search_params)
@@ -192,3 +272,5 @@ def get_similar_products_file(
         print("Product display name: {}".format(product.display_name))
         print("Product description: {}\n".format(product.description))
         print("Product labels: {}\n".format(product.product_labels))
+
+    return results
